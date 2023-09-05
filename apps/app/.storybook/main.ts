d43b7363915a4a327806283e4bcddecf232b37ed
Youssef <yousseftharwat.khalil@gmail.com>
2023-09-06 00:38:53 +0200

    feat(storybook): init

--- apps/app/.storybook/main.ts
+++ apps/app/.storybook/main.ts
@@ -1,13 +1,15 @@
-const config = {
-  stories: ['./src/components/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
-  addons: ['@storybook/addon-essentials', '@nx/react/plugins/storybook'],
+import type { StorybookConfig } from '@storybook/nextjs';
+
+const previewAnnotations: StorybookConfig = {
+  stories: ['../src/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
+  addons: ['@storybook/addon-essentials'],
   framework: {
     name: '@storybook/nextjs',
     options: {},
   },
 };
 
-export default config;
+export default previewAnnotations;
 
 // To customize your webpack configuration you can use the webpackFinal field.
 // Check https://storybook.js.org/docs/react/builders/webpack#extending-storybooks-webpack-config
