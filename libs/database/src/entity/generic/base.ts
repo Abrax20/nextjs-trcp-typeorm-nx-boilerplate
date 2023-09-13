d01601faba29adb028d048990f2760f5bd7821a7
Youssef <yousseftharwat.khalil@gmail.com>
2023-09-13 12:29:48 +0200

    refactor(db): base improve

--- libs/database/src/entity/generic/base.ts
+++ libs/database/src/entity/generic/base.ts
@@ -4,23 +4,54 @@ import {
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
-import { z } from 'zod';
+import { ZodError, z } from 'zod';
 
+/**
+ * MainEntity is a base entity that provides common fields and utilities for other entities.
+ */
 export class MainEntity extends BaseEntity {
+  /**
+   * UUID of the entity.
+   */
   @PrimaryGeneratedColumn('uuid')
   public uuid!: string;
 
-  @CreateDateColumn()
+  /**
+   * Timestamp when the entity was created.
+   */
+  @CreateDateColumn({ nullable: false })
   public createdAt!: Date;
 
-  @UpdateDateColumn()
+  /**
+   * Timestamp when the entity was last updated.
+   */
+  @UpdateDateColumn({ nullable: false })
   public updatedAt!: Date;
 
-  public changeField(key: keyof this, value: unknown, type: z.ZodType) {
+  /**
+   * Validates and changes a field's value based on a provided Zod schema.
+   * @param key - The field to change.
+   * @param value - The new value.
+   * @param type - The Zod schema to validate against.
+   * @returns True if the change was successful, false otherwise.
+   */
+  public changeField(
+    key: keyof this,
+    value: unknown,
+    type: z.ZodType<any, any>
+  ): boolean {
     try {
       this[key] = type.parse(value);
       return true;
-    } catch {
+    } catch (error) {
+      if (error instanceof ZodError) {
+        console.error(`Failed to update field ${String(key)}:`, error.errors);
+      } else {
+        console.error(
+          `Unexpected error when updating field ${String(key)}:`,
+          error
+        );
+      }
       return false;
     }
   }
